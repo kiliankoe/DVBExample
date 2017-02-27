ee5e4ea2df0d79f747cea3afd1eb8a8e7a5c17ff
Kilian Koeltzsch <me@kilian.io>
2017-02-27 14:20:31 +0100

    Implement basic Departure Monitor

--- DVBExample/ViewController.swift
+++ DVBExample/ViewController.swift
@@ -9,18 +9,58 @@
 import UIKit
 import DVB
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    let STOP_NAME = "Albertplatz"
+
+    var departures: [Departure]? {
+        didSet {
+            OperationQueue.main.addOperation {
+                self.tableView.reloadData()
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refresh))
+
+        self.navigationItem.title = STOP_NAME
+        refresh()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func refresh() {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        Departure.monitor(stopWithName: STOP_NAME) { result in
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+
+            guard let response = result.success else { return }
+            self.departures = response.departures
+        }
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let departures = departures else { return 0 }
+        return departures.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "departureCell") ?? UITableViewCell(style: .value1, reuseIdentifier: "departureCell")
+
+        guard let departures = departures else { return cell }
+
+        let departure = departures[indexPath.row]
+
+        cell.textLabel?.text = "\(departure.line) \(departure.direction)"
+        cell.detailTextLabel?.text = departure.fancyEta
+        cell.detailTextLabel?.textColor = departure.state == .delayed ? .red : .black
+//        cell.imageView?.image = UIImage(named: departure.mode.dvbIdentifier)
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
-
